9c5fa8449371a5b0519887b4c399a06e51645bd2
OJ999 <omarj819@gmail.com>
2023-10-19 05:04:55 -0700

    A

---
+++ more_functions_nested_loops/3-main.c
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype for the _putchar function */
+int _putchar(char c);
+
+/* Prototype for the print_numbers function */
+void print_numbers(void);
+
+#endif /* MAIN_H */
+
