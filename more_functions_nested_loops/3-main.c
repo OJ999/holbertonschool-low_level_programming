2f63eb8bf3fa0496b90f987d217076928408f2f2
OJ999 <omarj819@gmail.com>
2023-10-19 05:10:22 -0700

    A

--- more_functions_nested_loops/3-main.c
+++ more_functions_nested_loops/3-main.c
@@ -1,11 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#include "main.h"
 
-/* Prototype for the _putchar function */
-int _putchar(char c);
-
-/* Prototype for the print_numbers function */
-void print_numbers(void);
-
-#endif /* MAIN_H */
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
