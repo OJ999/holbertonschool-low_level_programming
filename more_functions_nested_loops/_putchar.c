4d4bd3a65d2ff597f1b1d636b64153afb14964aa
OJ999 <omarj819@gmail.com>
2023-10-19 05:13:29 -0700

    A

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9 followed by a newline.
+ */
+void print_numbers(void)
+{
+    char digit = '0';
+
+    while (digit <= '9')
+    {
+        _putchar(digit);
+        digit++;
+    }
+
+    _putchar('\n');
+}
+
