c7fbda5bee7b490361634b139ecdba47feb7beed
OJ999 <omarj819@gmail.com>
2023-10-19 02:01:52 -0700

    All

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,29 +3,29 @@
 /**
  * times_table - Prints the 9 times table, starting with 0
  */
+
 void times_table(void)
 {
-	int r, c, p;
+int r, c, p;
 	
-	for (r = 0; r < 10; r++)
-	{
-		for (c = 0; c < 10; c++)
-		{
-			p = r * c;
-			if (c == 0)
-				_putchar('0');
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (p < 10)
-					_putchar(' ');
-				else
-					_putchar((p / 10) + '0');
-				_putchar((p % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+for (r = 0; r < 10; r++)
+{
+for (c = 0; c < 10; c++)
+{
+p = r * c;
+if (c == 0)
+_putchar('0');
+else
+{
+_putchar(',');
+_putchar(' ');
+if (p < 10)
+_putchar(' ');
+else
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
 }
-
+}
+_putchar('\n');
+}
+}
