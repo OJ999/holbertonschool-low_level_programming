288fbefa514287b6f31ec1f539432279b482d5ed
OJ999 <omarj819@gmail.com>
2023-10-21 05:22:16 -0700

    A

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,10 @@
  */
 void print_numbers(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        _putchar(i + '0');  /* Convert the integer to character and print it */
-    }
-    
-    _putchar('\n');  /* Print a new line character to move to the next line */
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');  /* Convert the integer to character and print it */
 }
-
+_putchar('\n');  /* Print a new line character to move to the next line */
+}
