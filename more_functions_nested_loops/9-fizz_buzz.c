757b9b2bcefeaaf38de311b4d4a930010a933c44
OJ999 <omarj819@gmail.com>
2023-10-21 08:35:42 -0700

    A

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,35 +12,31 @@ int _putchar(char c)
 {
     return putchar(c); /* Use putchar from the standard library */
 }
-
 /**
  * fizz_buzz - Prints numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz rules
  */
 void fizz_buzz(void)
 {
-    int i;
-
-    for (i = 1; i <= 100; i++)
-    {
-	    if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz");
-        else if (i % 3 == 0)
-            printf("Fizz");
-        else if (i % 5 == 0)
-            printf("Buzz");
-        else
-            printf("%d", i);
-
-        if (i < 100)
-            printf(" ");
-        else
-            printf("\n");
-    }
+int i;
+for (i = 1; i <= 100; i++)
+}
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+else
+printf("\n");
+}
 }
 
 int main(void)
 {
-    fizz_buzz();
-    return (0);
-}
- 
+fizz_buzz();
+return (0);
+} 
