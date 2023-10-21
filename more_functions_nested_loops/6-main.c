902d1fb786be52d28e29c500569f724cb8a5e302
OJ999 <omarj819@gmail.com>
2023-10-21 07:00:43 -0700

    A

---
+++ more_functions_nested_loops/6-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+    print_line(5);
+    print_line(0);
+    print_line(3);
+
+    return (0);
+}
+
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line of underscores followed by a newline.
+ * @n: The number of underscores to print.
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+while (n > 0) 
+{
+_putchar('_');
+n--;
+}
+_putchar('\n');
+}
+}
