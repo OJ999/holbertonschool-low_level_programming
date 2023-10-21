f2a0bf9eee9dce0267dc3408efc09dbb46081e5b
OJ999 <omarj819@gmail.com>
2023-10-21 08:57:53 -0700

    A

--- more_functions_nested_loops/10-main.c
+++ more_functions_nested_loops/10-main.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
-int main(void) {
-    print_triangle(2);
-    print_triangle(10);
-    print_triangle(1);
-    print_triangle(0);
-    return (0);
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_triangle(0);
+	return (0);
 }
-
