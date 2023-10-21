c907738b4c625f7b8f7fdad53583b63f0ff22893
OJ999 <omarj819@gmail.com>
2023-10-21 08:00:09 -0700

    A

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,21 @@
-#include <unistd.h>
+#include "main.h"
 
-void _putchar(char c)
+int main(void)
 {
-write(1, &c, 1);
-}
-void print_fizz(void)
+for (int i = 1; i <= 100; i++)
 {
-_putchar('F');
-_putchar('i');
-_putchar('z');
-_putchar('z');
-}
-void print_buzz(void)
-{
-_putchar('B');
-_putchar('u');
-_putchar('z');
-_putchar('z');
-}
-void print_fizzbuzz(void)
-{
+if (i % 3 == 0 && i % 5 == 0)
+print_fizzbuzz();
+else if (i % 3 == 0)
 print_fizz();
+else if (i % 5 == 0)
 print_buzz();
-}
-void print_number(int n)
-{
-if (n < 10)
-_putchar(n + '0');
 else
-{
-print_number(n / 10);
-_putchar(n % 10 + '0');
+print_number(i);
+if (i < 100)
+print_space();
+else
+print_new_line();
 }
-}
-void print_space(void)
-{
-_putchar(' ');
-}
-void print_new_line(void)
-{
-_putchar('\n');
+return (0);
 }
