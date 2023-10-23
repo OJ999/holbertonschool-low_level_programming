e35b6e116ef59361f991f0e142c97fb4a358732e
OJ999 <omarj819@gmail.com>
2023-10-23 06:42:17 -0700

    A

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int _putchar(char c);
 void reset_to_98(int *n); /* Function prototype for reset_to_98 */
 
 int _putchar(char c);
-void swap_int(int *a, int *b); // Function prototype for swap_int
+void swap_int(int *a, int *b); /* Function prototype for swap_int */
 
 #endif /* MAIN_H */
 
