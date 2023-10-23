478eadaf31d42ad1abacf2aef28e0b5cd78d3283
OJ999 <omarj819@gmail.com>
2023-10-23 05:18:21 -0700

    A

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stddef.h> /* Include stddef.h to use NULL */
 
 void reset_to_98(int *n)
 {
 if (n != NULL)
 {
-*n = 98;  // Update the value pointed to by n to 98
+*n = 98;  /* Update the value pointed to by n to 98 */
 }
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n); // Function prototype for reset_to_98
+void reset_to_98(int *n); /* Function prototype for reset_to_98 */
 
 #endif /* MAIN_H */
 
