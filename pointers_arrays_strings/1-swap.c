4480d445a92e4720528b3f5031d508b8a45f298f
OJ999 <omarj819@gmail.com>
2023-10-23 06:39:36 -0700

    A

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;  /* Store the value pointed to by a in temp */
+*a = *b;    /* Update the value pointed to by a with the value pointed to by b */
+*b = temp;  /* Update the value pointed to by b with the value in temp */
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@
 int _putchar(char c);
 void reset_to_98(int *n); /* Function prototype for reset_to_98 */
 
+int _putchar(char c);
+void swap_int(int *a, int *b); // Function prototype for swap_int
+
 #endif /* MAIN_H */
 
