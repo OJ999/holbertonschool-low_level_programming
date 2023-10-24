6d16005ce1510549341ef780b757c13ba7644957
OJ999 <omarj819@gmail.com>
2023-10-24 09:12:27 -0700

    A

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,15 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i += 2;
-    }
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+if (str[i] != '\0')
+{
+i++;  // Move to the next character
+}
     _putchar('\n');
 }
