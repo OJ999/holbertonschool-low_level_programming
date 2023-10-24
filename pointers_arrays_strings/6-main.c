b51da0e0c79399fbd4fbae25fa388c27aa2ee31c
OJ999 <omarj819@gmail.com>
2023-10-24 02:41:40 -0700

    A

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -13,3 +13,12 @@ int main(void)
     puts2(str);
     return (0);
 }
+
+int main(void)
+{
+    char *str;
+
+    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    puts2(str);
+    return (0);
+}
