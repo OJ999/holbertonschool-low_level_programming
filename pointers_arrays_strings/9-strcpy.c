d4c3adc75c2835a58d2ba012da824156dc8f89ef
OJ999 <omarj819@gmail.com>
2023-10-24 09:46:52 -0700

    A

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy a string including the null byte.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: A pointer to the destination buffer (dest).
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+
+dest[i] = '\0';  // Add the null byte to the end of the destination string
+
+return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -29,6 +29,6 @@ void puts_half(char *str);
 int _putchar(char c);
 void print_array(int *a, int n);
 
-
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
