15d6211ab02e16ab019aa00bad49eca565892996
OJ999 <omarj819@gmail.com>
2023-10-24 02:40:06 -0700

    A

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -16,3 +16,11 @@ void puts2(char *str)
     _putchar('\n');
 }
 
+int main(void)
+{
+    char *str;
+
+    str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    puts2(str);
+    return (0);
+}
