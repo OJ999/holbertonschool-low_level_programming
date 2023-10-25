c03c976267bf5a57b727a8bfdeafc7fae8ad4788
OJ999 <omarj819@gmail.com>
2023-10-25 15:26:32 -0700

    A

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,30 +8,33 @@
  */
 int _atoi(char *s)
 {
-int sign = 1;  /* Initialize sign as positive */
-int result = 0;
-int i = 0;
+    int sign = 1;  /* Initialize sign as positive */
+    int result = 0;
+    int i = 0;
 
-/* Handle signs */
-while (s[i] == '-' || s[i] == '+') {
-if (s[i] == '-')
-sign *= -1;  /* Toggle the sign for negative */
-i++;
+    /* Handle signs */
+    while (s[i] == '-' || s[i] == '+') {
+        if (s[i] == '-')
+            sign *= -1;  /* Toggle the sign for negative */
+        i++;
+    }
+
+    /* Convert the string to an integer */
+    while (s[i] >= '0' && s[i] <= '9') {
+        int digit = s[i] - '0';
+
+        // Check for integer overflow for positive numbers
+        if (sign == 1 && (result > (INT_MAX - digit) / 10))
+            return -1;  // Overflow
+
+        // Check for integer overflow for negative numbers
+        if (sign == -1 && (result < (INT_MIN + digit) / 10))
+            return 0;  // Overflow
+
+        result = result * 10 + digit;
+        i++;
+    }
+
+    return result * sign;
 }
 
-/* Convert the string to an integer */
-while (s[i] >= '0' && s[i] <= '9') {
-int digit = s[i] - '0';
-
-/* Check for integer overflow */
-if (sign == 1 && (result > 214748364 || (result == 214748364 && digit > 7)))
-return (-1);
-if (sign == -1 && (result > 214748364 || (result == 214748364 && digit > 8)))
-return (0);
-
-result = result * 10 + digit;
-i++;
-}
-
-return (result * sign);
-}
