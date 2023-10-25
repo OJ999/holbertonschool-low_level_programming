12e9f843e5d73411a791e220846bc1cc1e716d45
OJ999 <omarj819@gmail.com>
2023-10-25 15:42:41 -0700

    A

--- pointers_arrays_strings/100-main.c
+++ pointers_arrays_strings/100-main.c
@@ -1,26 +1,34 @@
 #include "main.h"
 #include <stdio.h>
 
+int _atoi(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int nb;
+	int n;
 
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
+	n = _atoi("          ------++++++-----+++++--98");
+	printf("%d\n", n);
+	return (0);
+
+	n = _atoi("+++++    +-+    2242454");
+	printf("%d\n", n);
+	return (0);
+
+	n = _atoi("        +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA            ");
+	printf("%d\n", n);
+	return (0);
+
+	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+	printf("%d\n", n);
+	return (0);
+
+	n = _atoi("-2147483648");
+	printf("%d\n", n);
+	return (0);
 }
-
