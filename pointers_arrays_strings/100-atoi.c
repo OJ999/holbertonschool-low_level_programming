1c84239f1cd4852e2a548a9f40afd9a5f8e608a0
OJ999 <omarj819@gmail.com>
2023-10-27 06:28:41 -0700

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,37 +1,29 @@
 #include "main.h"
 
-/**
- * _atoi - Converts a string to an integer.
- * @s: The input string.
- *
- * Return: The integer value of the input string.
- */
 int _atoi(char *s)
 {
-int sign = 1;  /* Initialize sign as positive */
-int result = 0;
-int i = 0;
+    int result = 0;
+    int sign = 1; // Default to positive
 
-/* Handle signs */
-while (s[i] == '-' || s[i] == '+') {
-if (s[i] == '-')
-sign *= -1;  /* Toggle the sign for negative */
-i++;
+    // Skip leading whitespace
+    while (*s == ' ' || (*s >= 9 && *s <= 13))
+        s++;
+
+    // Handle sign
+    if (*s == '-') {
+        sign = -1;
+        s++;
+    } else if (*s == '+') {
+        s++;
+    }
+
+    // Convert digits to integer
+    while (*s >= '0' && *s <= '9') {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+
+    // Apply sign
+    return sign * result;
 }
 
-/* Convert the string to an integer */
-while (s[i] >= '0' && s[i] <= '9') {
-int digit = s[i] - '0';
-
-/* Check for integer overflow */
-if (sign == 1 && (result > 214748364 || (result == 214748364 && digit > 7)))
-return (-1);
-if (sign == -1 && (result > 214748364 || (result == 214748364 && digit > 8)))
-return (0);
-
-result = result * 10 + digit;
-i++;
-}
-
-return (result * sign);
-}
