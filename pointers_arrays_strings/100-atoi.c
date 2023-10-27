58f34a096fbba6f11bd4babd566da13d8814d2c9
OJ999 <omarj819@gmail.com>
2023-10-27 09:37:22 -0700

    A

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
-int _atoi(char *s) {
+int _atoi(char *s) /* Intro */
+{
 int result = 0; /* Initialize the result */
 int sign = 1;   /* Initialize the sign as positive (default) */
 int foundDigit = 0; /* Flag to check if at least one digit is found */
 
 /* Parse the string to convert it to an integer */
-while (*s) 
+while (*s)
 {
-if (*s == '-' || *s == '+') 
+if (*s == '-' || *s == '+')
 {
-/* Check for multiple signs, stop parsing if more than one sign is encountered */
+/* Check for mul signs, stop parsing more than one sign encountered */
 if (foundDigit)
 {
 break;
@@ -41,12 +42,12 @@ foundDigit = 1; /* Set the flag to indicate at least one digit found */
 }
 else if (foundDigit)
 {
-/* Stop parsing if a non-digit character is encountered after at least one digit */
+/* Stop parsing non-digit character encountered after one digit */
 break;
 }
 s++; /* Move to the next character */
 }
 
 /* Apply the sign to the result */
-return (result * sign);
+return (result *sign);
 }
