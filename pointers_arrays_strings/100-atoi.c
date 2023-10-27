db698b55f9eb3acafd82999ebc6d723ceb6badbb
OJ999 <omarj819@gmail.com>
2023-10-27 09:04:29 -0700

    A

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,46 +3,34 @@
 
 int _atoi(char *s)
 {
-    int sign = 1;  /* Initialize sign as positive */
     int result = 0;
+    int sign = 1;
     int i = 0;
-    int numStarted = 0; /* Indicates if numeric characters have started */
-    int is_min = 0; /* Flag to indicate INT_MIN case */
-    int digit; /* Declare digit outside the loop */
-
-    /* Handle signs and skip leading non-numeric characters */
-    while (s[i] != '\0') {
-        if (s[i] == '-' || s[i] == '+') {
-            if (numStarted) {
-                break; /* If numeric characters have started, stop at signs */
-            } else {
-                if (s[i] == '-')
-                    sign *= -1;  /* Toggle the sign for negative */
-            }
-        } else if (s[i] >= '0' && s[i] <= '9') {
-            numStarted = 1; /* Numeric characters have started */
-            digit = s[i] - '0';
-
-            if (result < (INT_MIN + digit) / 10) {
-                return INT_MIN; /* Overflow, set to INT_MIN */
-            } else if (result == (INT_MIN + digit) / 10) {
-                if (sign == -1) {
-                    is_min = 1; /* Handle INT_MIN case */
-                }
-            } else if (result > INT_MAX / 10) {
-                return INT_MAX; /* Overflow, set to INT_MAX */
-            }
-
-            result = result * 10 - digit;
-        } else {
-            if (numStarted) {
-                break; /* If numeric characters have started, stop at non-numeric characters */
-            }
-        }
-
+    
+    // Skip leading white spaces
+    while (s[i] == ' ' || s[i] == '\t')
+        i++;
+    
+    // Check for an optional sign
+    if (s[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (s[i] == '+') {
         i++;
     }
-
-    return is_min ? INT_MIN : result * sign;
+    
+    // Process digits
+    while (s[i] >= '0' && s[i] <= '9') {
+        int digit = s[i] - '0';
+        
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10)) {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        
+        result = result * 10 + digit;
+        i++;
+    }
+    
+    return result * sign;
 }
 
