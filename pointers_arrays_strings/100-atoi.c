ea85579b1ddc2f857540dfd3dcdc276c852d64f9
OJ999 <omarj819@gmail.com>
2023-10-27 06:46:02 -0700

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,6 +7,7 @@ int _atoi(char *s)
     int result = 0;
     int i = 0;
     int numStarted = 0; /* Indicates if numeric characters have started */
+    int digit; // Declare digit outside the loop
 
     /* Handle signs and skip leading non-numeric characters */
     while (s[i] != '\0') {
@@ -19,7 +20,7 @@ int _atoi(char *s)
             }
         } else if (s[i] >= '0' && s[i] <= '9') {
             numStarted = 1; /* Numeric characters have started */
-            int digit = s[i] - '0';
+            digit = s[i] - '0';
 
             /* Check for integer overflow for positive numbers */
             if (sign == 1 && (result > (INT_MAX - digit) / 10))
