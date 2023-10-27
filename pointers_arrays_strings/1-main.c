d537d2d8ff07608208bd53199186b9c41aa15f6a
OJ999 <omarj819@gmail.com>
2023-10-27 10:39:39 -0700

    A

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -1,15 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int a = 98;
-    int b = 42;
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
 
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
     return (0);
 }
-
---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The maximum number of bytes to append from src
+ * Return: A pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+    int dest_len = 0;
+    int i = 0;
+
+    // Calculate the length of the dest string
+    while (dest[dest_len] != '\0')
+    {
+        dest_len++;
+    }
+
+    // Append at most n bytes from src to the dest string
+    while (src[i] != '\0' && i < n)
+    {
+        dest[dest_len] = src[i];
+        dest_len++;
+        i++;
+    }
+
+    dest[dest_len] = '\0'; // Add the null-terminating character
+
+    return dest;
+}
+
