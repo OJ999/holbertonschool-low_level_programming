3369560338fb31eea9aee12d094f60f7966291ff
OJ999 <omarj819@gmail.com>
2023-10-27 07:04:22 -0700

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,44 +3,55 @@
 
 int _atoi(char *s)
 {
-    int sign = 1;  /* Initialize sign as positive */
-    int result = 0;
-    int i = 0;
-    int numStarted = 0; /* Indicates if numeric characters have started */
-    int is_min = 0; /* Flag to indicate INT_MIN case */
-    int digit; /* Declare digit outside the loop */
+int sign = 1;  /* Initialize sign as positive */
+int result = 0;
+int i = 0;
+int numStarted = 0; /* Indicates if numeric characters have started */
+int is_min = 0; /* Flag to indicate INT_MIN case */
+int digit; /* Declare digit outside the loop */
 
-    /* Handle signs and skip leading non-numeric characters */
-    while (s[i] != '\0') {
-        if (s[i] == '-' || s[i] == '+') {
-            if (numStarted) {
-                break; /* If numeric characters have started, stop at signs */
-            } else {
-                if (s[i] == '-')
-                    sign *= -1;  /* Toggle the sign for negative */
-            }
-        } else if (s[i] >= '0' && s[i] <= '9') {
-            numStarted = 1; /* Numeric characters have started */
-            digit = s[i] - '0';
+/* Handle signs and skip leading non-numeric characters */
+while (s[i] != '\0')
+{
+if (s[i] == '-' || s[i] == '+')
+{
+if (numStarted)
+{
+break; /* If numeric characters have started, stop at signs */
+}
+else
+{
+if (s[i] == '-')
+sign *= -1;  /* Toggle the sign for negative */
+}
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+numStarted = 1; /* Numeric characters have started */
+digit = s[i] - '0';
 
-            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && digit > 8)) {
-                return INT_MIN; /* Overflow, set to INT_MIN */
-            }
-
-            result = result * 10 - digit; // Note the subtraction here
-
-            if (result == INT_MIN) {
-                is_min = 1; /* Handle INT_MIN case */
-            }
-        } else {
-            if (numStarted) {
-                break; /* If numeric characters have started, stop at non-numeric characters */
-            }
-        }
-
-        i++;
-    }
-
-    return is_min ? INT_MIN : result * sign;
+if (result == INT_MIN / 10 && digit == 8 && sign == -1)
+{
+is_min = 1; /* Handle INT_MIN case */
 }
 
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > 7))
+{
+return (is_min ? INT_MIN : INT_MAX); /* Overflow */
+}
+
+result = result * 10 + digit;
+}
+else
+{
+if (numStarted)
+{
+break; /* If numeric characters have started, stop at non-numeric characters */
+}
+}
+
+i++;
+}
+
+return (result * sign);
+}
