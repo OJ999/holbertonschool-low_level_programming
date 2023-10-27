ffc091231cdd6a303317afa31fb646fe97c9b157
OJ999 <omarj819@gmail.com>
2023-10-27 06:31:18 -0700

    a

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,29 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
-int _atoi(char *s)
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    int result = 0;
-    int sign = 1; // Default to positive
+    int nb;
 
-    // Skip leading whitespace
-    while (*s == ' ' || (*s >= 9 && *s <= 13))
-        s++;
-
-    // Handle sign
-    if (*s == '-') {
-        sign = -1;
-        s++;
-    } else if (*s == '+') {
-        s++;
-    }
-
-    // Convert digits to integer
-    while (*s >= '0' && *s <= '9') {
-        result = result * 10 + (*s - '0');
-        s++;
-    }
-
-    // Apply sign
-    return sign * result;
+    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);
+    return (0);
 }
 
