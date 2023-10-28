cd9697ae6e1c6f49f7652825d483cc1b6da5eed2
OJ999 <omarj819@gmail.com>
2023-10-28 03:24:08 -0700

    A

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  * _strchr - Locates a character in a string
  * @s: The string to be searched
  * @c: The character to be found in s
- * Return: Pointer to the first occurrence of the character c in the string s, or NULL if not found
+ * Return: Pointer to first occurrence of c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
