827e6754a69eebd816bb819f6eb429e0ebd245f5
OJ999 <omarj819@gmail.com>
2023-10-28 03:51:06 -0700

    A

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: The string to be searched
  * @accept: The bytes to search for
- * Return: Pointer s byte that matches accept bytes, or NULL if no such byte is found
+ * Return: Pointer s byte that matches accept bytes, or NULL if no byte found
  */
 char *_strpbrk(char *s, char *accept)
 {
