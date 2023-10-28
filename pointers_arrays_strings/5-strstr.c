63710da5a62c4ee00898cf68108ea0d077c9b087
OJ999 <omarj819@gmail.com>
2023-10-28 04:05:25 -0700

    A

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strstr - Locates a substring.
  * @haystack: The string to be searched
  * @needle: The substring to be found
- * Return: Pointer to the beginning of the located substring, or NULL if not found
+ * Return: Pointer to the beginning of the located substring, or 0 if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -25,6 +25,5 @@ return (start);
 haystack = start + 1;
 }
 
-/* Returning NULL to indicate not found */
-return (NULL);
+return (0); // Returning 0 to indicate not found
 }
