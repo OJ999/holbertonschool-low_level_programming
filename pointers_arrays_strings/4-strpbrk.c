16d699210d6bf04649e15e50f3cecec2c7206e90
OJ999 <omarj819@gmail.com>
2023-10-28 03:46:46 -0700

    A

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,26 +1,28 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: The string to be searched
  * @accept: The bytes to search for
- * Return: Pointer to s byte that matches accept bytes, or NULL if no such byte is found
+ * Return: Pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-while (*s)
-{
-char *ptr = accept;
-while (*ptr)
-{
-if (*s == *ptr)
-{
-return (s);
-}
-ptr++;
-}
-s++;
+    while (*s)
+    {
+        char *ptr = accept;
+        while (*ptr)
+        {
+            if (*s == *ptr)
+            {
+                return s;
+            }
+            ptr++;
+        }
+        s++;
+    }
+
+    return NULL;
 }
 
-return (NULL);
-}
