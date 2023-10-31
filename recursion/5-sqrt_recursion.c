47784cc54d1dcf5ea9bcc606e2571ac997b577a0
OJ999 <omarj819@gmail.com>
2023-10-31 08:22:22 -0700

    A

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,48 +1,47 @@
 #include "main.h"
 
 /**
- * _sqrt_recursive - Returns the natural square root of a number.
+ * sqrt_recursive - Recursively finds the natural square root of a number.
  * @n: The number to find the square root of.
  * @i: The current value to check if it's the square root of n.
- * Return: Square root n. If n doesn't have natural square root, returns -1.
+ *
+ * Return: The square root of n. If n doesn't have a natural square root, returns -1.
  */
-
-int sqrt_recursive(int n, int i) /* sqrt_recursive function */
+int sqrt_recursive(int n, int i)
 {
-/* Base cases */
-if (i * i == n)
-{
-return (i); /* If square of i equals n, i is the square root */
-}
-else if (i * i > n)
-{
-return (-1); /* If square i < n, n doesn't have natural square root */
-}
-else
-{
-return (sqrt_recursive(n, i + 1)); /* Recursively search for the square root */
-}
+    if (i * i == n)
+    {
+        return i; /* If the square of i equals n, i is the square root */
+    }
+    else if (i * i > n)
+    {
+        return -1; /* If the square of i is greater than n, n doesn't have a natural square root */
+    }
+    else
+    {
+        return sqrt_recursive(n, i + 1); /* Recursively search for the square root */
+    }
 }
 
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number to find the square root of.
  *
- * Return: Square root of n. If n doesn't have natural square root, return -1.
+ * Return: The square root of n. If n does not have a natural square root, returns -1.
  */
-
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-{
-return (-1); /* Return -1 if n negative (no square root negative numbers) */
+    if (n < 0)
+    {
+        return -1; /* Return -1 if n is negative (no square root for negative numbers) */
+    }
+    else if (n == 0)
+    {
+        return 0; /* Square root of 0 is 0 */
+    }
+    else
+    {
+        return sqrt_recursive(n, 1); /* Start recursive search from 1 */
+    }
 }
-else if (n == 0)
-{
-return (0); /* Square root of 0 is 0 */
-}
-else
-{
-return (sqrt_recursive(n, 1)); /* Start recursive search from 1 */
-}
-}
+
