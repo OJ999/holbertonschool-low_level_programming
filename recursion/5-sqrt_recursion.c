48d23bddbf15e927ed245aa570ea2991664fe171
OJ999 <omarj819@gmail.com>
2023-10-31 08:26:54 -0700

    A

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,22 +5,22 @@
  * @n: The number to find the square root of.
  * @i: The current value to check if it's the square root of n.
  *
- * Return: The square root of n. If n doesn't have a natural square root, returns -1.
+ * Return: Square root n. If n doesn't have natural square root, returns -1.
  */
 int sqrt_recursive(int n, int i)
 {
-    if (i * i == n)
-    {
-        return i; /* If the square of i equals n, i is the square root */
-    }
-    else if (i * i > n)
-    {
-        return -1; /* If the square of i is greater than n, n doesn't have a natural square root */
-    }
-    else
-    {
-        return sqrt_recursive(n, i + 1); /* Recursively search for the square root */
-    }
+if (i * i == n)
+{
+return (i); /* If the square of i equals n, i is the square root */
+}
+else if (i * i > n)
+{
+return (-1); /* If the square of i is < n, n doesn't have natural square root */
+}
+else
+{
+return (sqrt_recursive(n, i + 1)); /* Recursively search for the square root */
+}
 }
 
 /**
@@ -31,17 +31,17 @@ int sqrt_recursive(int n, int i)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return -1; /* Return -1 if n is negative (no square root for negative numbers) */
-    }
-    else if (n == 0)
-    {
-        return 0; /* Square root of 0 is 0 */
-    }
-    else
-    {
-        return sqrt_recursive(n, 1); /* Start recursive search from 1 */
-    }
+if (n < 0)
+{
+return (-1); /* Return -1 if n is negative (no square root for negative numbers) */
+}
+else if (n == 0)
+{
+return (0); /* Square root of 0 is 0 */
+}
+else
+{
+return (sqrt_recursive(n, 1)); /* Start recursive search from 1 */
+}
 }
 
