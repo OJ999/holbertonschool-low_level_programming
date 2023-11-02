26f5d605ee7d2a5b54afb3f3c21d09b1fac34a8a
OJ999 <omarj819@gmail.com>
2023-11-02 04:32:04 -0700

    A

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    (void)argc;  // To silence the unused variable warning
+    /* To silence the unused variable warning */
+    (void)argc;  
     printf("%s\n", argv[0]);  // Printing the program name
 
     return 0;
