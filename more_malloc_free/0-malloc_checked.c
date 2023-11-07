30799dc70780d3cbef8030717383aa72ef00b88c
OJ999 <omarj819@gmail.com>
2023-11-07 07:52:29 -0800

    A

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "main.h"  // Ensure to include the header file with prototypes
+#include "main.h"  /* Ensure to include the header file with prototypes */
 
 void *malloc_checked(unsigned int b)
 {
@@ -7,7 +7,7 @@ void *ptr = malloc(b);
 
 if (ptr == NULL)
 {
-exit(98); // Terminate the process with status 98 if malloc fails
+exit(98); /* Terminate the process with status 98 if malloc fails */
 }
 
 return (ptr);
