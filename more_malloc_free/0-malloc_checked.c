a64b86f78338da039339804beca56f9dc5284ed8
OJ999 <omarj819@gmail.com>
2023-11-07 07:53:57 -0800

    A

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include "main.h"  /* Ensure to include the header file with prototypes */
 
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The amount of memory to allocate
+ *
+ * Return: A pointer to the allocated memory
+ * If malloc fails, the program terminates with status value 98
+ */
+
 void *malloc_checked(unsigned int b)
 {
 void *ptr = malloc(b);
