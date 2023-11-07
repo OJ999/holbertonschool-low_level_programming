284f0e0c68c92b4e414b74fe02470b47c2baf998
OJ999 <omarj819@gmail.com>
2023-11-07 07:50:58 -0800

    A

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"  // Ensure to include the header file with prototypes
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+{
+exit(98); // Terminate the process with status 98 if malloc fails
+}
+
+return (ptr);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Header file with function prototypes */
+
+void *malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
+
