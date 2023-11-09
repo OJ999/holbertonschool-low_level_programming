2c328aeff3605d0d511f438a8f19e3b2412f4f26
OJ999 <omarj819@gmail.com>
2023-11-09 05:33:06 -0800

    A

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the file it was compiled from.
+ *
+ * Return: Always 0
+ */
+
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
-
