51ec1f9eaca08af1739785e7320b3ad68bd11558
Omar J <omarj819@gmail.com>
2023-11-13 19:44:58 +0200

    A

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -5,23 +5,23 @@
 
 int main(void)
 {
-    // Create a new dog
+    /* Create a new dog */
     dog_t *my_dog = new_dog("Ghost", 4.75, "Jon Snow");
 
-    // Check if the new_dog function failed
+    /* Check if the new_dog function failed */
     if (my_dog == NULL)
     {
         fprintf(stderr, "Failed to create a new dog.\n");
         return 1;
     }
 
-    // Print information about the dog
+    /* Print information about the dog */
     printf("My name is %s, I am %.2f, and my owner is %s.\n", my_dog->name, my_dog->age, my_dog->owner);
 
-    // Free the allocated memory
+    /* Free the allocated memory */
     free(my_dog->name);
     free(my_dog->owner);
     free(my_dog);
 
-    return 0;
+    return (0);
 }
