54a0957ebefacaa20adb65d63888dc00194c3902
Omar J <omarj819@gmail.com>
2023-11-13 18:10:33 +0200

    A

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-// Implementation of the function as previously provided
+/* Implementation of the function as previously provided */
 dog_t *new_dog;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
