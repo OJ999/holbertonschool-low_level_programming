9b85b546b5f9581a404eb51175cf5be8db044a58
Omar J <omarj819@gmail.com>
2023-11-13 20:04:10 +0200

    A

--- structures_typedef/4-main.c
+++ structures_typedef/4-main.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    /* Create a new dog */
-    dog_t *my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+    dog_t *my_dog;
 
-    /* Check if the new_dog function failed */
-    if (my_dog == NULL)
-    {
-        fprintf(stderr, "Failed to create a new dog.\n");
-        return 1;
-    }
-
-    /* Print information about the dog */
-    printf("My name is %s, I am %.2f, and my owner is %s.\n", my_dog->name, my_dog->age, my_dog->owner);
-
-    /* Free the allocated memory */
-    free(my_dog->name);
-    free(my_dog->owner);
-    free(my_dog);
-
-    return 0;
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    return (0);
 }
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,40 +4,40 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog with specified name, age, and owner.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Return: Pointer to the newly created dog, or NULL if the function fails.
+ * Return: pointer to the newly created dog_t struct, or NULL on failure.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog;
+    dog_t *new_dog;
+    char *name_copy, *owner_copy;
 
-/* Allocate memory for the new dog structure */
-new_dog = malloc(sizeof(dog_t));
-if (new_dog == NULL)
-return (NULL);
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return NULL;
 
-/* Allocate memory for the name and owner, and copy the values */
-new_dog->name = strdup(name);
-if (new_dog->name == NULL)
-{
-free(new_dog);
-return (NULL);
-}
+    name_copy = malloc(strlen(name) + 1);
+    if (name_copy == NULL) {
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(name_copy, name);
 
-new_dog->owner = strdup(owner);
-if (new_dog->owner == NULL)
-{
-free(new_dog->name);
-free(new_dog);
-return (NULL);
-}
+    owner_copy = malloc(strlen(owner) + 1);
+    if (owner_copy == NULL) {
+        free(name_copy);
+        free(new_dog);
+        return NULL;
+    }
+    strcpy(owner_copy, owner);
 
-/* Set the age of the dog */
-new_dog->age = age;
+    new_dog->name = name_copy;
+    new_dog->age = age;
+    new_dog->owner = owner_copy;
 
-return (new_dog);
+    return new_dog;
 }
