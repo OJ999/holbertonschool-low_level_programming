32dfb50b54fc7c02fb49e7cd23cdb9ce64ccce04
Omar J <omarj819@gmail.com>
2023-11-13 19:23:48 +0200

    A

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,6 +19,7 @@ dog_t *new_dog;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
+
 /* Allocate memory for the name and owner, and copy the values */
 new_dog->name = strdup(name);
 if (new_dog->name == NULL)
@@ -36,5 +37,26 @@ return (NULL);
 
 /* Set the age of the dog */
 new_dog->age = age;
+
 return (new_dog);
+}
+
+/* Example usage in main */
+int main(void)
+{
+dog_t *my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+if (my_dog == NULL)
+{
+fprintf(stderr, "Failed to create a new dog.\n");
+return 1;
+}
+
+printf("My name is %s, I am %.2f, and my owner is %s.\n", my_dog->name, my_dog->age, my_dog->owner);
+
+/* Don't forget to free the memory before exiting the program */
+free(my_dog->name);
+free(my_dog->owner);
+free(my_dog);
+
+return 0;
 }
