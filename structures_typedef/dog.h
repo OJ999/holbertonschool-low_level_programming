fa899936ebea09f9d3832cee6d4782429204297c
Omar J <omarj819@gmail.com>
2023-11-13 19:16:49 +0200

    A

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,31 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog - Represents information about a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-struct dog {
-    char *name;
-    float age;
-    char *owner;
-};
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-/* Forward declaration of struct dog */
-struct dog;
 
 /**
  * struct dog - Structure representing a dog
