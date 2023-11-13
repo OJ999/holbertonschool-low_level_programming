c1157de5908a93f6e45b49fe9bf64f3c7008d1b1
Omar J <omarj819@gmail.com>
2023-11-13 19:58:04 +0200

    A

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -41,23 +41,3 @@ new_dog->age = age;
 
 return (new_dog);
 }
-
-/**
- * main - check the code .
- *
- * Return: Always 0.
- */
-int main(void)
-{
-dog_t *my_dog;
-
-my_dog = new_dog("Ghost", 4.75, "Jon Snow");
-if (my_dog == NULL)
-{
-printf("Failed\n");
-return (1);
-}
-printf("My name is %s, I am %.2f, and my owner is %s\n",
-my_dog->name, my_dog->age, my_dog->owner);
-return (0);
-}
