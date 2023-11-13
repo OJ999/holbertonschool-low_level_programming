f0b47f87d624cf257ffb1f91f54abe95874bc196
Omar J <omarj819@gmail.com>
2023-11-13 19:34:27 +0200

    A

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,5 +19,18 @@ void print_dog(struct dog *d);
 
 /* New function prototype for creating a new dog */
 dog_t *new_dog(char *name, float age, char *owner);
+int main(void)
+{
+dog_t *my_dog;
 
+my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+if (my_dog == NULL)
+{
+printf("Failed\n");
+return (1);
+}
+printf("My name is %s, I am %.2f, and my owner is %s\n",
+my_dog->name, my_dog->age, my_dog->owner);
+return (0);
+}
 #endif /* DOG_H */
