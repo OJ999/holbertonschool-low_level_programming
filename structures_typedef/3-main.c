f6292fa6cb7ac7a09ca035bf43016c76d3b47fce
Omar J <omarj819@gmail.com>
2023-11-13 17:43:01 +0200

    A

--- structures_typedef/3-main.c
+++ structures_typedef/3-main.c
@@ -2,17 +2,23 @@
 #include "dog.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    dog_t my_dog;
+#ifdef dog_t
+	printf("'dog_t' should not be a #define !\n");
+	return (0);
+#else
+	dog_t my_dog;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	my_dog.name = "Ghost";
+	my_dog.age = 4.75;
+	my_dog.owner = "Jon Snow";
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+		my_dog.name, my_dog.age, my_dog.owner);
+	return (0);
+#endif
 }
