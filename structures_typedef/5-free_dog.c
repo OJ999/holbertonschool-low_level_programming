64ea044db177b311c321c60f857142ecb647b675
Omar J <omarj819@gmail.com>
2023-11-13 20:20:48 +0200

    A

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,8 +9,8 @@ void free_dog(dog_t *d)
 {
 if (d != NULL)
 {
-free(d->name); // Free the memory allocated for the name
-free(d->owner); // Free the memory allocated for the owner
-free(d); // Free the memory allocated for the structure itself
+free(d->name); /* Free the memory allocated for the name */
+free(d->owner); /* Free the memory allocated for the owner */
+free(d); /* Free the memory allocated for the structure itself */
 }
 }
