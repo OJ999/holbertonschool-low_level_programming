652e24979c0608ec7492e71946f6ead305bef19a
Omar J <omarj819@gmail.com>
2023-11-13 19:14:28 +0200

    A

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -24,4 +24,26 @@ void print_dog(struct dog *d);
 
 dog_t *new_dog(char *name, float age, char *owner);
 
+/* Forward declaration of struct dog */
+struct dog;
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/* New function prototype for creating a new dog */
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif /* DOG_H */
