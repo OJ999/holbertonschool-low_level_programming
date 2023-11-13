67aa0d3719e14dea3beec6d71c00751d03e6ba43
Omar J <omarj819@gmail.com>
2023-11-13 17:41:40 +0200

    A

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,4 +17,12 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+typedef struct dog dog_t;
+
+
 #endif /* DOG_H */
