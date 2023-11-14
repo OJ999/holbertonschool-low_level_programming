bb8c48f0103e365f8d2f98a3e580201892b1a4f9
Omar J <omarj819@gmail.com>
2023-11-14 16:49:34 +0200

    A

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,39 +12,39 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    unsigned int i = 0;
-    char *str;
+va_list args;
+unsigned int i = 0;
+char *str;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s", str);
-                break;
-        }
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
+break;
+}
 
-        if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[i + 1] != '\0')
-            printf(", ");
+if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') && format[i + 1] != '\0')
+printf(", ");
 
-        i++;
-    }
+i++;
+}
 
-    va_end(args);
-    printf("\n");
+va_end(args);
+printf("\n");
 }
