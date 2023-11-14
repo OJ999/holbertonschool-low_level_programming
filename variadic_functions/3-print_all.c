afeb54b4a5874a7abd8d5514d73a2a05b844b0e5
Omar J <omarj819@gmail.com>
2023-11-14 16:41:27 +0200

    A

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -8,39 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-unsigned int i = 0;
-char *separator = "";
-char *str;
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    int first_arg = 1;  // Variable to track if we are printing the first argument
 
-va_start(args, format);
+    va_start(args, format);
 
-while (format && format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%s%c", separator, va_arg(args, int));
-break;
-case 'i':
-printf("%s%d", separator, va_arg(args, int));
-break;
-case 'f':
-printf("%s%f", separator, va_arg(args, double));
-break;
-case 's':
-str = va_arg(args, char *);
-if (str == NULL)
-str = "(nil)";
-printf("%s%s", separator, str);
-break;
-}
+    while (format && format[i])
+    {
+        // Print comma only if not the first argument
+        if (!first_arg)
+            printf(", ");
 
-separator = ", ";
-i++;
-}
+        switch (format[i])
+        {
+            case 'c':
+                printf("%c", va_arg(args, int));
+                break;
+            case 'i':
+                printf("%d", va_arg(args, int));
+                break;
+            case 'f':
+                printf("%f", va_arg(args, double));
+                break;
+            case 's':
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(nil)";
+                printf("%s", str);
+                break;
+        }
 
-printf("\n");
+        first_arg = 0;  // After printing the first argument, set to 0
+        i++;
+    }
 
-va_end(args);
+    printf("\n");
+
+    va_end(args);
 }
