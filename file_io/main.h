d97d60ddb5c14f84cd6f804236f4271d804cf489
Omar J <omarj819@gmail.com>
2023-12-05 20:40:33 +0200

    A

--- file_io/main.h
+++ file_io/main.h
@@ -3,7 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
+int create_file(const char *filename, char *text_content);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 
