309af64b84ccfaf22de999844621acfd49b8a281
Omar J <omarj819@gmail.com>
2023-12-05 22:39:18 +0200

    A

---
+++ hash_tables/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "hash_tables.h"  // Include the necessary header files
+
+// Declare prototypes for functions used in the main program
+void someFunction(void);
+
+#endif /* MAIN_H */
