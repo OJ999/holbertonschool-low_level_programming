7482aba5b592b57c083c608d3af98a96a6ac4d0d
Omar J <omarj819@gmail.com>
2023-12-05 21:13:26 +0200

    A

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,52 +6,38 @@
 
 #define BUFFER_SIZE 1024
 
-/**
- * main - Copies the content of a file to another file.
- * @argc: The number of arguments.
- * @argv: The array of arguments.
- *
- * Return: 0 on success, or the corresponding error code on failure.
- */
-int main(int argc, char **argv)
-{
+int main(int argc, char *argv[]) {
     int file_from, file_to, read_chars, write_chars;
     char buffer[BUFFER_SIZE];
     struct stat st;
 
-    if (argc != 3)
-    {
+    if (argc != 3) {
         dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
         return 97;
     }
 
     file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1)
-    {
+    if (file_from == -1) {
         dprintf(2, "Error: Can't read from file %s\n", argv[1]);
         return 98;
     }
 
-    if (fstat(file_from, &st) == -1)
-    {
-        close(file_from);
+    if (fstat(file_from, &st) == -1) {
         perror("Error");
+        close(file_from);
         return 98;
     }
 
-    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode);
-    if (file_to == -1)
-    {
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode & 0777);
+    if (file_to == -1) {
         dprintf(2, "Error: Can't write to %s\n", argv[2]);
         close(file_from);
         return 99;
     }
 
-    do
-    {
+    do {
         read_chars = read(file_from, buffer, BUFFER_SIZE);
-        if (read_chars == -1)
-        {
+        if (read_chars == -1) {
             dprintf(2, "Error: Can't read from file %s\n", argv[1]);
             close(file_from);
             close(file_to);
@@ -59,8 +45,7 @@ int main(int argc, char **argv)
         }
 
         write_chars = write(file_to, buffer, read_chars);
-        if (write_chars == -1 || write_chars != read_chars)
-        {
+        if (write_chars == -1 || write_chars != read_chars) {
             dprintf(2, "Error: Can't write to %s\n", argv[2]);
             close(file_from);
             close(file_to);
@@ -68,8 +53,7 @@ int main(int argc, char **argv)
         }
     } while (read_chars > 0);
 
-    if (close(file_from) == -1 || close(file_to) == -1)
-    {
+    if (close(file_from) == -1 || close(file_to) == -1) {
         perror("Error");
         return 100;
     }
