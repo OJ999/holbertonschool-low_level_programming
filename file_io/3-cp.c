a915ca4839e07c24ff3f9b23969002d99e752d91
Omar J <omarj819@gmail.com>
2023-12-05 21:32:45 +0200

    A

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,26 +13,29 @@ void print_error(int code, const char *msg, const char *filename) {
 }
 
 int main(int argc, char *argv[]) {
+    const char *file_from;
+    const char *file_to;
+    int fd_from, fd_to;
+    char buffer[BUFFER_SIZE];
+    ssize_t read_bytes, write_bytes;
+
     if (argc != 3) {
         print_error(97, "Usage: %s file_from file_to\n", argv[0]);
     }
 
-    const char *file_from = argv[1];
-    const char *file_to = argv[2];
+    file_from = argv[1];
+    file_to = argv[2];
 
-    int fd_from = open(file_from, O_RDONLY);
+    fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1) {
         print_error(98, "Error: Can't read from file %s\n", file_from);
     }
 
-    int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
     if (fd_to == -1) {
         close(fd_from);
         print_error(99, "Error: Can't write to %s\n", file_to);
     }
-
-    char buffer[BUFFER_SIZE];
-    ssize_t read_bytes, write_bytes;
 
     do {
         read_bytes = read(fd_from, buffer, BUFFER_SIZE);
@@ -51,7 +54,7 @@ int main(int argc, char *argv[]) {
     } while (read_bytes > 0);
 
     if (close(fd_from) == -1 || close(fd_to) == -1) {
-        print_error(100, "Error: Can't close fd %d\n", fd_to);
+        print_error(100, "Error: Can't close fd %d\n", (const char *)fd_to);
     }
 
     return 0;
