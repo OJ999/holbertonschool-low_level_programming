2fb17bf84585b45fcec4f21f2bb4726fa93b064c
Omar J <omarj819@gmail.com>
2023-12-05 21:57:49 +0200

    A

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -42,4 +42,53 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
 
 	return (EXIT_SUCCESS);
+}/**
+ * main - program that copies the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: file to copy.
+ *
+ * Return: 1: success or -1: failure.
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, fd1read, fd2write;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+/* */
+	do {
+		fd1read = read(fd1, buf, 1024);
+		if (fd1read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+/* */
+		fd2write = write(fd2, buf, fd1read);
+		if (fd2write == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	} while (fd1read == 1024);
+
+	if (close(fd1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
